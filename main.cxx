f530479d6073664ce66deaf932851f3d51e3a7f9
Matthew Wysocki <matthew.g.wysocki+git@gmail.com>
2014-11-18 15:39:15 -0500

    updates for Qt5

--- main.cxx
+++ main.cxx
@@ -1,6 +1,6 @@
 
 #include <QApplication>
-#include <QPlastiqueStyle>
+//#include <QPlastiqueStyle>
 
 #include "MainWindow.h"
 
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
 
-#ifdef Q_WS_X11
-  // Why the hell is Motif my default style on Linux?
-  // Might be fixed in Qt 4.2
-  //QApplication::setStyle(new QPlastiqueStyle);
-#endif
+// #ifdef Q_WS_X11
+//   // Why the hell is Motif my default style on Linux?
+//   // Might be fixed in Qt 4.2
+//   //QApplication::setStyle(new QPlastiqueStyle);
+// #endif
 
   MainWindow main_window;
   main_window.show();
